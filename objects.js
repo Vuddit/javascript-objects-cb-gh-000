173b51a57e38154fa0834a25762a89fd971976a2
Rohit Kale <ruk22@cam.ac.uk>
2020-04-28 22:53:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({ artist: "song" })
 
-function updatePlaylist(object, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 }
 
