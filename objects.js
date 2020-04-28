4415ae796284a3b0c4d30e8a6d9098bd326ff6a0
Rohit Kale <ruk22@cam.ac.uk>
2020-04-28 22:55:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist["artist"];
+function removeFromPlaylist(playlist, songwriter) {
+  delete playlist["songwriter"];
   console.log(playlist)
 }
