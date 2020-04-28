15d090f7a5698053a2e061d3588e087f46f656db
Rohit Kale <ruk22@cam.ac.uk>
2020-04-28 23:13:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(obj, key, value) {
 }
 
 function removeFromPlaylist (playlist, artistName) {
-	delete playlist.Slowdive;
+	delete playlist[artistName]
 	console.log(playlist);
 }
